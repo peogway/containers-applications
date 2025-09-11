c219a4a62340d5e60ed5e8bb1ebe0d8dbbf64239
peogway <duchungbeo2004@gmail.com>
2025-09-11 08:17:48 +0300

    using separate todo view component

--- todo-app/todo-frontend/src/Todos/List.jsx
+++ todo-app/todo-frontend/src/Todos/List.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import SingleTodo from './SingleTodo'
 
 const TodoList = ({ todos, deleteTodo, completeTodo }) => {
 	const onClickDelete = (todo) => () => {
@@ -12,40 +12,13 @@ const TodoList = ({ todos, deleteTodo, completeTodo }) => {
 	return (
 		<>
 			{todos.map((todo, idx) => {
-				const doneInfo = (
-					<>
-						<span>This todo is done</span>
-						<span>
-							<button onClick={onClickDelete(todo)}> Delete </button>
-						</span>
-					</>
-				)
-
-				const notDoneInfo = (
-					<>
-						<span>This todo is not done</span>
-						<span>
-							<button onClick={onClickDelete(todo)}> Delete </button>
-							<button onClick={onClickComplete(todo)}> Set as done </button>
-						</span>
-					</>
-				)
-
 				return (
-					<React.Fragment key={todo._id}>
-						<div
-							style={{
-								display: 'flex',
-								justifyContent: 'space-between',
-								maxWidth: '70%',
-								margin: 'auto',
-							}}
-						>
-							<span>{todo.text}</span>
-							{todo.done ? doneInfo : notDoneInfo}
-						</div>
-						<hr key={`sep-${todo._id}`} />
-					</React.Fragment>
+					<SingleTodo
+						key={todo._id}
+						todo={todo}
+						onClickDelete={onClickDelete}
+						onClickComplete={onClickComplete}
+					/>
 				)
 			})}
 		</>
